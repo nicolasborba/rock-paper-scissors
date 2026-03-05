90faac13091109c37528302f85e531d3ff065392
nicolasborba <78428173+nicolasborba@users.noreply.github.com>
2026-03-05 06:27:16 -0300

    Create the basic logic for the human choice

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 let computerChoice;
 
-let getComputerChoice = function() {
+function getComputerChoice() {
 
   // find a random number between 1 and 3
   let randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -19,6 +19,27 @@ let getComputerChoice = function() {
 
 }
 
-
 computerChoice = getComputerChoice();
 console.log(computerChoice);
+
+let humanChoice;
+let humanNumber
+
+function getHumanChoice() {
+
+  humanNumber = prompt("Do you choose Rock, Paper or Scissors? Enter 1 for Rock, 2 for Paper and 3 for Scissors");
+  humanNumber = Number(humanNumber);
+  console.log(humanNumber);
+
+  switch (humanNumber) {
+    case 1:
+      return "Rock";
+    case 2:
+      return "Paper";
+    default:
+      return "Scissors";
+  }
+}
+
+humanChoice = getHumanChoice();
+console.log(humanChoice);
