7175501c882450c876c1c6fb426653ba7848c7eb
nicolasborba <78428173+nicolasborba@users.noreply.github.com>
2026-03-05 05:50:08 -0300

    Prepare random number generator

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+let computerChoice;
+
+let getComputerChoice = function() {
+
+  // find a random number between 1 and 3
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  console.log(randomNumber);
+
+
+
+
+
+
+}
+
+
+getComputerChoice();
